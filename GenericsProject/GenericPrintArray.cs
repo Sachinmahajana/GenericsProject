ce2ea3bf6584f96b9720dec8dbf2ac9ce777f06b
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-07 15:37:54 +0530

    [Sachin] Adding the UC2-GenericArray

---
+++ GenericsProject/GenericPrintArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProject
+{
+    internal class GenericPrintArray
+    {
+        public static void ToPrint<T>(T[] array)
+        {
+            Console.WriteLine("\nDisplaying values");
+            foreach(T element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
--- GenericsProject/Program.cs
+++ GenericsProject/Program.cs
@@ -10,13 +10,28 @@ namespace GenericsProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Generics Programs");
             int[] intArray = { 20, 30, 40, };
             double[] doubleArray = { 22.3, 20.4, 30.2 };
             char[] charArray = { 'A', 'B', 'C' };
 
-            PrintArray.ToPrint(intArray);
-            PrintArray.ToPrint(doubleArray);
-            PrintArray.ToPrint(charArray);
+            Console.WriteLine("PLease Choose a program from below options");
+            Console.WriteLine("1.PrintArray\n2.PrintArray by using Generic method\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    PrintArray.ToPrint(intArray);
+                    PrintArray.ToPrint(doubleArray);
+                    PrintArray.ToPrint(charArray);
+                    break;
+                case 2:
+                    GenericPrintArray.ToPrint<int>(intArray);
+                    GenericPrintArray.ToPrint<double>(doubleArray);
+                    GenericPrintArray.ToPrint<char>(charArray);
+                    break;
+            }
             Console.ReadLine();
         }
     }
