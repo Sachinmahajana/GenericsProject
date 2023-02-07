24ab3051ec405041a651b43337b0d7348d461d58
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-07 14:54:47 +0530

    [Sachin] Adding the UC1-PrintArray

---
+++ GenericsProject/PrintArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProject
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] array)
+        {
+            Console.WriteLine("\nDisplaying integer numbers");
+            foreach (int element in array)
+            {
+                Console.Write(element+" ");
+            }
+        }
+        public static void ToPrint(double[] array)
+        {
+            Console.WriteLine("\nDisplaying double values");
+            foreach (double element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+        public static void ToPrint(char[] sachin)
+        {
+            Console.WriteLine("\nDisplaying char values");
+            foreach (char element in sachin)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
--- GenericsProject/Program.cs
+++ GenericsProject/Program.cs
@@ -10,6 +10,14 @@ namespace GenericsProject
     {
         static void Main(string[] args)
         {
+            int[] intArray = { 20, 30, 40, };
+            double[] doubleArray = { 22.3, 20.4, 30.2 };
+            char[] charArray = { 'A', 'B', 'C' };
+
+            PrintArray.ToPrint(intArray);
+            PrintArray.ToPrint(doubleArray);
+            PrintArray.ToPrint(charArray);
+            Console.ReadLine();
         }
     }
 }
